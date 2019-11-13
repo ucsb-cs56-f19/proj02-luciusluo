be489ee75b080dfa4ae46ef9ca90bde1e8a17b8a
Yuxiao Luo <yuxiaoluo@ucsb.edu>
2019-11-12 21:16:59 -0800

    fix javadoc param and return issues

--- src/main/java/hello/GithubOrgMembershipService.java
+++ src/main/java/hello/GithubOrgMembershipService.java
@@ -52,10 +52,10 @@ public class GithubOrgMembershipService implements MembershipService {
     /**
      * is current logged in user has role
      * 
-     * @param roleToTest "member" or "admin"
+     * @param oauthToken oauth Token
+     * @param roleToTest role to test
      * @return if the current logged in user has that role
      */
-
     public boolean hasRole(OAuth2AuthenticationToken oauthToken, String roleToTest) {
         if (oauthToken == null) {
             return false;
