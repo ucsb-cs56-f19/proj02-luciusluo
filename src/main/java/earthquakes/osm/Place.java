eafcec2c03978d5a02f2abc6ccee11e2ac57aa61
Lucius2000 <lucius0228@gmail.com>
2019-12-03 13:19:27 -0800

    yl - fixed json mismatch

--- src/main/java/earthquakes/osm/Place.java
+++ src/main/java/earthquakes/osm/Place.java
@@ -1,4 +1,4 @@
-package earthquakes.geojson;
+package earthquakes.osm;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -6,29 +6,35 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import java.util.List;
+import com.fasterxml.jackson.core.type.TypeReference;
 
-public class FeatureCollection {
+public class Place {
+  public long place_id;
+  public double lat;
+  public double lon;
+  public String display_name;
   public String type;
-  private static Logger logger = LoggerFactory.getLogger(FeatureCollection.class);
-  public Metadata metadata;
-  public List<Feature> features;
-    
+  private static Logger logger = LoggerFactory.getLogger(Place.class);
+
+
   /**
      * Create a FeatureCollection object from json representation
-     *
+     * 
      * @param json String of json returned by API endpoint {@code /classes/search}
      * @return a new FeatureCollection object
      * @see <a href=
      *      "https://tools.ietf.org/html/rfc7946">https://tools.ietf.org/html/rfc7946</a>
      */
-    public static FeatureCollection fromJSON(String json) {
+
+    public static List<Place> listFromJSON(String json) {
         try {
             ObjectMapper objectMapper = new ObjectMapper();
             objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 
-            FeatureCollection featureCollection = objectMapper.readValue(json, FeatureCollection.class);
-            
-            return featureCollection;
+            // FeatureCollection featureCollection = objectMapper.readValue(json, FeatureCollection.class);
+            // List<Place> place = objectMapper.readValue(json, objectMapper.getTypeFactory().constructCollectionType(List.class, Place.class));
+            List<Place> place = objectMapper.readValue(json, new TypeReference<List<Place>>(){});
+            return place;
         } catch (JsonProcessingException jpe) {
             logger.error("JsonProcessingException:" + jpe);
             return null;
